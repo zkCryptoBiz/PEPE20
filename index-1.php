538bb7680e9c4a9c4e479047d0fce2bd68db18be
zkCryptoBiz <158302791+zkCryptoBiz@users.noreply.github.com>
2025-02-24 18:57:16 +0200

    Add files via upload

---
+++ index-1.php
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en-gb">
+	<title type="text">PEPE 2.0 - THE LEGEND OF THE SEQUEL</title>
+	<subtitle type="text">Get ready to embark on an electrifying journey into the future with Pepe 2.0, the much-awaited sequel to the groundbreaking Pepe project! Brace yourself for an </subtitle>
+	<link rel="alternate" type="text/html" href="https://pepe20.vip"/>
+	<id>https://pepe20.vip/index.php</id>
+	<updated>2025-02-24T13:05:06+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="https://pepe20.vip/index.php?format=feed&amp;type=atom"/>
+</feed>
